db72dde357be380d51969ecc567caa1bc7c0e70c
Ivan Monichev <ivan@monichev.ru>
2023-03-31 22:23:14 +0300

    feat: add card markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,362 @@
-import React from 'react';
 
-function App() {
+
+function App(): JSX.Element {
   return (
-    <div className="App">
-    </div>
+    <>
+      <header className="header">
+        <h1 className="header__title">Cars List</h1>
+      </header>
+      <main className="main">
+        <section className='main__item cars'>
+          <h2 className='visually-hidden'>Список машин</h2>
+          <ul className="cars-list">
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+            <li className="cars-list__item">
+              <article className="cars-card">
+                <h2 className='cars-card__name'>Toyota</h2>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Модель: </span>Camry
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Год:</span> 2021
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цвет:</span> red
+                </p>
+                <p className='cars-card__text'>
+                  <span className='secondary-text'>Цена:</span> 21000
+                </p>
+                <p className="cars-card__text">
+                  <span className='secondary-text'>Координаты:</span> 55.753215, 37.620393
+                </p>
+              </article>
+            </li>
+          </ul>
+        </section>
+        <section className='main__item map'>
+          <h2 className='visually-hidden'>Карта</h2>
+        </section>
+      </main>
+    </>
   );
 }
 
